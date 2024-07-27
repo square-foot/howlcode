3a624701cb9df0e5cc91ac0ec56179942ffafa2e
Sabu Francis <sf@sabufrancis.com>
2024-07-27 13:33:52 +0530

    First commit for Github

---
+++ HowlGenerators/php/init.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+@REM This batch file is invoked just before generation. The full -dirbuild folder is passed to this batch file
+@REM Use this to do some initialization of the backend server, if so required
+@REM In case of some backends; e.g. PHP and Pocketbase, there is nothing to be done with this batch file
